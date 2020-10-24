15bc284e835a370e8bf056f27b8293c5a08fca70
Patrick José Pereira <patrickelectric@gmail.com>
2020-10-24 14:22:55 -0300

    main: Update SSL handle
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ import http.server
 import os
 import ssl
 
-source_dir = os.path.join(os.path.dirname(__file__), 'source')
+script_dir = os.path.abspath(os.path.dirname(__file__))
+source_dir = os.path.join(script_dir, "source")
 os.chdir(source_dir)
+
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def send_error(self, code, message=None):
         if code == 404:
-            self.error_message_format = open("404.html","r").read()
+            self.error_message_format = open("404.html", "r").read()
         http.server.SimpleHTTPRequestHandler.send_error(self, code, message)
 
-address = ('localhost', 8000)
+
+address = ("localhost", 8000)
 httpd = http.server.HTTPServer(address, Handler)
 try:
-    httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
-except:
-    print("No SSL configuration, webapp not available.")
+    httpd.socket = ssl.wrap_socket(
+        httpd.socket,
+        certfile=os.path.join(script_dir, "cert.pem"),
+        keyfile=os.path.join(script_dir, "key.pem"),
+        server_side=True,
+    )
+except Exception as error:
+    print(f"No SSL configuration, webapp not available: {error}")
 print(f"Console available in: {address}")
 httpd.serve_forever()
