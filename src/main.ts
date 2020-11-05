f6153e1b5e02f3892b169d05a0f69900c37ff28b
Patrick José Pereira <patrickelectric@gmail.com>
2020-11-05 16:21:18 -0300

    src: Register service-worker
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,9 @@ import App from "./App.vue"
 createApp(App).mount("#app")
 
 export default App
+
+if("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("service-worker.js")
+        .then(function() { console.log("Service Worker Registered.") })
+}
