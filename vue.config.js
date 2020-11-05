1a5991bd61aed236985f121db4d9882be015adcb
Patrick José Pereira <patrickelectric@gmail.com>
2020-11-05 10:37:47 -0300

    Configure website color and apple webapp capability
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
     publicPath: process.env.PUBLIC_PATH === ""
         ? "/"
-        : process.env.PUBLIC_PATH
+        : process.env.PUBLIC_PATH,
+
+    pwa: {
+        themeColor: "#2686c3",
+        msTileColor: "#ffffff",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "white",
+        name: "MGroundStation",
+        manifestOptions: {
+            background_color: "#2686c3"
+        }
+    }
 }
